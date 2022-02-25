9851e7ebebe80004880121e97537b42d483a12da
Andre Gallant <andre@gallantandre.com>
2022-02-25 10:02:19 -0400

    Add more integer types

--- blast/blast.hpp
+++ blast/blast.hpp
@@ -4,9 +4,13 @@
 //--- Basic types used everywhere ---
 namespace blast {
 
-using u32 = uint32_t;
-using i32 = int32_t;
-using real = double; // for testing float vs double speed and precision
+using u8    = uint8_t;
+using u16   = uint16_t;
+using u32   = uint32_t;
+using u8    = int8_t;
+using i16   = int16_t;
+using i32   = int32_t;
+using real  = double; // for testing float vs double speed and precision
 
 } // namespace blast
 
