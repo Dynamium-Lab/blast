307c6e3e3cb8f0b18ff96a6d53022c83009496a1
Andre Gallant <gallantandre@gmail.com>
2022-06-07 10:13:14 -0300

    Gen3 7dof kinematics and dynamics (not validated)

--- blast/blast_manipulator.hpp
+++ blast/blast_manipulator.hpp
@@ -80,9 +80,28 @@ struct Gen3Lite : public Manipulator {
     virtual void dynamics(Matrix& pos, Matrix& vel, Matrix& acc, Matrix& efforts) override;
 
     // compute forward kinematics for 1 point
-    Array Gen3Lite::forward_kinematics(Array& joint_position);
+    Array forward_kinematics(Array& joint_position);
 };
 
+struct Gen3_7DOF : public Manipulator {
+    Vec3 p_base;
+    real m[7];
+    Mat3 I[7];
+    Vec3 av[7];
+    Vec3 dv[7];
+    Vec3 sv[7];
+    Vec3 ev[7];
+
+    // default constructor
+    Gen3_7DOF();
+
+    // compute joint torque as a function of trajector (pva)
+    virtual void dynamics(Pva& pva, Matrix& efforts) override;
+    virtual void dynamics(Matrix& pos, Matrix& vel, Matrix& acc, Matrix& efforts) override;
+
+    // compute forward kinematics for 1 point
+    Array forward_kinematics(Array& joint_position);
+};
 
 
 
@@ -702,6 +721,278 @@ inline Array Gen3Lite::forward_kinematics(Array& joint_position) {
     p_tmp += (Q_tmp*=Q4)*dv[3];
     p_tmp += (Q_tmp*=Q5)*dv[4];
     p_tmp += (Q_tmp*=Q6)*dv[5];
+    p_tmp += p_base;
+
+    Array pose(6);
+    pose[0] = p_tmp.x;
+    pose[1] = p_tmp.y;
+    pose[2] = p_tmp.z;
+    // todo: Q_tmp(0, 0) and Q_tmp(2, 2) must not be zero!!
+    pose[3] = atan2(Q_tmp(1, 0), Q_tmp(0, 0));
+    pose[4] = atan2(-Q_tmp(2, 0), sqrt(Q_tmp(2, 1)*Q_tmp(2, 1) + Q_tmp(2, 2)*Q_tmp(2, 2)));
+    pose[5] = atan2(Q_tmp(2, 1), Q_tmp(2, 2));
+    return pose;
+}
+
+//------ Kinova Gen3 7DOF manipulator functions --------------------------------------------------------------------------
+
+inline Gen3_7DOF::Gen3_7DOF() : Manipulator(7) {
+    // position of the first joint with respect to the table in the center of the base
+    p_base = {0, 0, 0.1564};
+
+    // mass
+    m[0] = 1.377;
+    m[1] = 1.1636;
+    m[2] = 1.1636;
+    m[3] = 0.93;
+    m[4] = 0.678;
+    m[5] = 0.678;
+    m[6] = 0.364;
+
+    // inertial tensor
+    I[0] = {0.004570, 0.000001, 0.000002, 0.000001, 0.004831, 0.000448, 0.000002, 0.000448, 0.001409};
+    I[1] = {0.011088, 0.000005, 0.000000, 0.000005, 0.001072, 0.000691, 0.000000, 0.000691, 0.011255};
+    I[2] = {0.010932, 0.000000, 0.000007, 0.000000, 0.011127, 0.000606, 0.000007, 0.000606, 0.001043};
+    I[3] = {0.008147, 0.000001, 0.000000, 0.000001, 0.000631, 0.000500, 0.000000, 0.000500, 0.008316};
+    I[4] = {0.001596, 0.000000, 0.000000, 0.000000, 0.001607, 0.000256, 0.000000, 0.000256, 0.000399};
+    I[5] = {0.001641, 0.000000, 0.000000, 0.000000, 0.000410, 0.000278, 0.000000, 0.000278, 0.001641};
+    I[6] = {0.000214, 0.000000, 0.000001, 0.000000, 0.000223, 0.000002, 0.000001, 0.000002, 0.000240};
+
+    // center of mass
+    av[0] = {-0.000023, -0.010364, -0.073360};
+    av[1] = {-0.000044, -0.099580, -0.013278};
+    av[2] = {-0.000044, -0.006641, -0.117892};
+    av[3] = {-0.000018, -0.075478, -0.015006};
+    av[4] = { 0.000001, -0.009432, -0.063883};
+    av[5] = { 0.000001, -0.045483, -0.009650};
+    av[6] = {-0.000093,  0.000132, -0.022905};
+
+    // vector to next joint
+    dv[0] = { 0.0,  0.0054, -0.1284};
+    dv[1] = { 0.0, -0.2104,  0.0064};
+    dv[2] = { 0.0, -0.0064, -0.2104};
+    dv[3] = { 0.0, -0.2084, -0.0064};
+    dv[4] = { 0.0,  0.0,    -0.1059};
+    dv[5] = { 0.0, -0.1059,  0.0};
+    dv[6] = { 0.0,  0.0,    -0.0615};
+
+    // center of mass (from next joint)
+    sv[0] = dv[0] - av[0];
+    sv[1] = dv[1] - av[1];
+    sv[2] = dv[2] - av[2];
+    sv[3] = dv[3] - av[3];
+    sv[4] = dv[4] - av[4];
+    sv[5] = dv[5] - av[5];
+    sv[6] = dv[6] - av[6];
+
+    // unit joint direction
+    ev[0] = { 0,  0,  -1};
+    ev[1] = { 0, -1,  0};
+    ev[2] = { 0,  1,  0};
+    ev[3] = { 0, -1,  0};
+    ev[4] = { 0,  1,  0};
+    ev[5] = { 0, -1,  0};
+    ev[6] = { 0,  1,  0};
+
+    // kinematic and dynamic constraints
+    pmax = {2.69, 2.69, 2.69, 2.59, 2.57, 2.59}; // rad
+    vmax = {1.74533, 1.74533, 1.74533, 1.74533, 3.141593, 3.141593}; // rad/s
+    umax = {56, 56, 56, 56, 29, 29, 29};    // Nm
+    vmin = -vmax;
+    pmin = -pmax;
+    umin = -umax;
+}
+
+inline void Gen3_7DOF::dynamics(Pva& pva, Matrix& efforts) {
+    dynamics(pva.pos, pva.vel, pva.acc, efforts);
+}
+
+inline void Gen3_7DOF::dynamics(Matrix& pos, Matrix& vel, Matrix& acc, Matrix& efforts) {
+
+    Mat3 Q1, Q2, Q3, Q4, Q5, Q6, Q7;
+    Mat3 Q1t, Q2t, Q3t, Q4t, Q5t, Q6t, Q7t;
+    Vec3 w12, w23, w34, w45, w56, w67, w78;
+    Vec3 wd12, wd23, wd34, wd45, wd56, wd67, wd78;
+    Vec3 cdd01, cdd12, cdd23, cdd34, cdd45, cdd56, cdd67, cdd78;
+    cdd01.z = 9.81;
+    Vec3 f1, f2, f3, f4, f5, f6, f7;
+    Vec3 n1, n2, n3, n4, n5, n6, n7;
+
+    // loop all points
+    for (u32 i = 0; i < pos.cols; i++) {
+        auto v = &vel(0, i);
+        auto a = &acc(0, i);
+
+        // SIMD compute sines and cosines note: approx 10% faster
+        auto p = &pos(0, i);
+        real s[8];
+        real c[8];
+        // first 4
+        {
+            __m256d s_tmp;
+            __m256d c_tmp;
+            __m256d angle_v = _mm256_load_pd(p);
+            s_tmp = _mm256_sincos_pd(&c_tmp, angle_v);
+            _mm256_storeu_pd(s, s_tmp);
+            _mm256_storeu_pd(c, c_tmp);
+        }
+        // 5, 6 and 7 (note: 8 is computed but never used)
+        {
+            __m256d s_tmp;
+            __m256d c_tmp;
+            __m256d angle_v = _mm256_load_pd(p+4);
+            s_tmp = _mm256_sincos_pd(&c_tmp, angle_v);
+            _mm256_storeu_pd(s+4, s_tmp);
+            _mm256_storeu_pd(c+4, c_tmp);
+        }
+
+        // note: these are stored column-wise
+        Q1 = {c[0], -s[0],  0,        -s[0], -c[0],   0,        0,  0,  1};
+        Q2 = {c[1],   0,   s[1],      -s[1],   0,    c[1],      0, -1,  0};
+        Q3 = {c[2],   0,  -s[2],      -s[2],   0,   -c[2],      0,  1,  0};
+        Q4 = {c[3],   0,   s[3],      -s[3],   0,    c[3],      0, -1,  0};
+        Q5 = {c[4],   0,  -s[4],      -s[4],   0,   -c[4],      0,  1,  0};
+        Q6 = {c[5],   0,   s[5],      -s[5],   0,    c[5],      0, -1,  0};
+        Q7 = {c[6],   0,  -s[6],      -s[6],   0,   -c[6],      0,  1,  0};
+        Q1t = transpose(Q1);
+        Q2t = transpose(Q2);
+        Q3t = transpose(Q3);
+        Q4t = transpose(Q4);
+        Q5t = transpose(Q5);
+        Q6t = transpose(Q6);
+        Q7t = transpose(Q7);
+
+        // note: this is the Newton algorithm in 'Element de robotique' course notes.
+        //-- kinematics
+        w12 = v[0]*ev[0];
+        w23 = Q2t*w12 + v[1]*ev[1];
+        w34 = Q3t*w23 + v[2]*ev[2];
+        w45 = Q4t*w34 + v[3]*ev[3];
+        w56 = Q5t*w45 + v[4]*ev[4];
+        w67 = Q6t*w56 + v[5]*ev[5];
+        w78 = Q7t*w67 + v[6]*ev[6];
+
+        wd12 = a[0]*ev[0];
+        cdd12 = Q1t*cdd01
+                + cross(wd12, av[0])
+                + cross(w12, cross(w12, av[0]));
+
+        wd23 = Q2t*wd12 + a[1]*ev[1] + v[1]*cross(Q2t*w12, ev[1]);
+        cdd23 = Q2t*cdd12
+                + cross(wd23, av[1])
+                + cross(w23, cross(w23, av[1]))
+                - Q2t*cross(wd12, sv[0])
+                - Q2t*cross(w12, cross(w12, sv[0]));
+
+        wd34 = Q3t*wd23 + a[2]*ev[2] + v[2]*cross(Q3t*w23, ev[2]);
+        cdd34 = Q3t*cdd23
+                + cross(wd34, av[2])
+                + cross(w34, cross(w34, av[2]))
+                - Q3t*cross(wd23, sv[1])
+                - Q3t*cross(w23, cross(w23, sv[1]));
+
+        wd45 = Q4t*wd34 + a[3]*ev[3] + v[3]*cross(Q4t*w34, ev[3]);
+        cdd45 = Q4t*cdd34
+                + cross(wd45, av[3])
+                + cross(w45, cross(w45, av[3]))
+                - Q4t*cross(wd34, sv[2])
+                - Q4t*cross(w34, cross(w34, sv[2]));
+
+        wd56 = Q5t*wd45 + a[4]*ev[4] + v[4]*cross(Q5t*w45, ev[4]);
+        cdd56 = Q5t*cdd45
+                + cross(wd56, av[4])
+                + cross(w56, cross(w56, av[4]))
+                - Q5t*cross(wd45, sv[3])
+                - Q5t*cross(w45, cross(w45, sv[3]));
+
+        wd67 = Q6t*wd56 + a[5]*ev[5] + v[5]*cross(Q6t*w56, ev[5]);
+        cdd67 = Q6t*cdd56
+                + cross(wd67, av[5])
+                + cross(w67, cross(w67, av[5]))
+                - Q6t*cross(wd56, sv[4])
+                - Q6t*cross(w56, cross(w56, sv[4]));
+
+        wd78 = Q7t*wd67 + a[6]*ev[6] + v[6]*cross(Q7t*w67, ev[6]);
+        cdd78 = Q7t*cdd67
+                + cross(wd78, av[6])
+                + cross(w78, cross(w78, av[6]))
+                - Q7t*cross(wd67, sv[5])
+                - Q7t*cross(w67, cross(w67, sv[5]));
+
+        //-- dynamics
+        f7 = Q7*(m[6]*cdd78);
+        n7 = Q7*(I[6]*wd78 + cross(w78, I[6]*w78) + cross(av[6], Q7t*f7));
+
+        f6 = Q6*(m[5]*cdd67 + f7);
+        n6 = Q6*(I[5]*wd67 + cross(w67, I[5]*w67) + n7 + cross(av[5], Q6t*f6) - cross(sv[5], f7));
+
+        f5 = Q5*(m[4]*cdd56 + f6);
+        n5 = Q5*(I[4]*wd56 + cross(w56, I[4]*w56) + n6 + cross(av[4], Q5t*f5) - cross(sv[4], f6));
+
+        f4 = Q4*(m[3]*cdd45 + f5);
+        n4 = Q4*(I[3]*wd45 + cross(w45, I[3]*w45) + n5 + cross(av[3], Q4t*f4) - cross(sv[3], f5));
+
+        f3 = Q3*(m[2]*cdd34 + f4);
+        n3 = Q3*(I[2]*wd34 + cross(w34, I[2]*w34) + n4 + cross(av[2], Q3t*f3) - cross(sv[2], f4));
+
+        f2 = Q2*(m[1]*cdd23 + f3);
+        n2 = Q2*(I[1]*wd23 + cross(w23, I[1]*w23) + n3 + cross(av[1], Q2t*f2) - cross(sv[1], f3));
+
+        f1 = Q1*(m[0]*cdd12 + f2);
+        n1 = Q1*(I[0]*wd12 + cross(w12, I[0]*w12) + n2 + cross(av[0], Q1t*f1) - cross(sv[0], f2));
+
+        //-- extract torques (last element of each moment vector)
+        efforts(0, i) = n1.z;
+        efforts(1, i) = n2.z;
+        efforts(2, i) = n3.z;
+        efforts(3, i) = n4.z;
+        efforts(4, i) = n5.z;
+        efforts(5, i) = n6.z;
+        efforts(6, i) = n7.z;
+    }
+}
+
+inline Array Gen3_7DOF::forward_kinematics(Array& joint_position) {
+    real s[8];
+    real c[8];
+    auto p = joint_position.data;
+    Mat3 Q1, Q2, Q3, Q4, Q5, Q6, Q7;
+    // first 4
+    {
+        __m256d s_tmp;
+        __m256d c_tmp;
+        __m256d angle_v = _mm256_load_pd(p);
+        s_tmp = _mm256_sincos_pd(&c_tmp, angle_v);
+        _mm256_storeu_pd(s, s_tmp);
+        _mm256_storeu_pd(c, c_tmp);
+    }
+    // 5, 6 and 7 (note: 8 is computed but never used)
+    {
+        __m256d s_tmp;
+        __m256d c_tmp;
+        __m256d angle_v = _mm256_load_pd(p+4);
+        s_tmp = _mm256_sincos_pd(&c_tmp, angle_v);
+        _mm256_storeu_pd(s+4, s_tmp);
+        _mm256_storeu_pd(c+4, c_tmp);
+    }
+
+    // note: these are stored column-wise
+    Q1 = {c[0], -s[0],  0,        -s[0], -c[0],   0,        0,  0,  1};
+    Q2 = {c[1],   0,   s[1],      -s[1],   0,    c[1],      0, -1,  0};
+    Q3 = {c[2],   0,  -s[2],      -s[2],   0,   -c[2],      0,  1,  0};
+    Q4 = {c[3],   0,   s[3],      -s[3],   0,    c[3],      0, -1,  0};
+    Q5 = {c[4],   0,  -s[4],      -s[4],   0,   -c[4],      0,  1,  0};
+    Q6 = {c[5],   0,   s[5],      -s[5],   0,    c[5],      0, -1,  0};
+    Q7 = {c[6],   0,  -s[6],      -s[6],   0,   -c[6],      0,  1,  0};
+
+    auto Q_tmp = Q1;
+    auto p_tmp = Q_tmp*dv[0];
+    p_tmp += (Q_tmp*=Q2)*dv[1];
+    p_tmp += (Q_tmp*=Q3)*dv[2];
+    p_tmp += (Q_tmp*=Q4)*dv[3];
+    p_tmp += (Q_tmp*=Q5)*dv[4];
+    p_tmp += (Q_tmp*=Q6)*dv[5];
+    p_tmp += (Q_tmp*=Q7)*dv[6];
     p_tmp += p_base;
 
     Array pose(6);
