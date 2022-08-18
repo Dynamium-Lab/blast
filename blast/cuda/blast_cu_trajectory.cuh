ee564311872f20893cea14d262a8873b0c9bdb2c
Andre Gallant <gallantandre@gmail.com>
2022-08-18 14:51:47 -0300

    Cuda dynamics function for Kinova Gen3

--- blast/cuda/blast_cu_trajectory.cuh
+++ blast/cuda/blast_cu_trajectory.cuh
@@ -5,8 +5,22 @@
 
 namespace blast {
 
-struct cuPva {
+
+__device__
+struct cuPvaBspline {
+    unsigned dof;
+    unsigned npoints;
+    unsigned p;
+    unsigned ncontrol;
+
+
+    // compute the basis functions
+    __host__ void init();
+
+    // map to GPU using cudaMemcpy
+    __host__ void map_to_gpu(double* arena);
 
 };
+
 
 } // namespace blast
--- blast/cuda/blast_cuda.cuh
+++ blast/cuda/blast_cuda.cuh
@@ -1,10 +1,6 @@
 #pragma once
 
 #include <cuda_runtime.h>
-#include <device_launch_parameters.h>
-#ifdef __INTELLISENSE__
-void __syncthreads();
-#endif
 
 #include "cuda/blast_cu_math.cuh"
 #include "cuda/blast_cu_utils.cuh"
@@ -19,14 +15,4 @@ __global__ void test_kernal();
 
 __global__ void test_kernal() {
     blast::cuGen3_7DOF* manip = (blast::cuGen3_7DOF*)blast::manip_broadcast_arena;
-    if (blockIdx.x == 0) {
-        printf("%f\n", manip->pmax[0]);
-        printf("%f\n", manip->pmax[1]);
-        printf("%f\n", manip->pmax[2]);
-        printf("%f\n", manip->pmax[3]);
-        printf("%f\n", manip->pmax[4]);
-        printf("%f\n", manip->pmax[5]);
-        printf("%f\n", manip->pmax[6]);
-    }
-    __syncthreads();
 }
