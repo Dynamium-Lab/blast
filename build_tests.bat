c501ab4d307981c045a6341e2930124e0cfa67b7
Andre Gallant <gallantandre@umoncton.ca>
2023-03-23 15:35:01 -0300

    Compiler optimization options for compilation of tests

--- build_tests.bat
+++ build_tests.bat
@@ -4,7 +4,8 @@ echo 'building tests and benchmarks'
 pushd "examples/build"
 
 set includes=-I "../../blast/" -I "../../blast/optional"
-set options= --std c++17 -m64 -O0 -g -D BLAST_DEBUG
+@REM set options= --std c++17 -m64 -O0 -g -D BLAST_DEBUG
+set options= --std c++17 -m64 -O3
 nvcc ../tests.cu -o tests.exe %includes% %options% %libr%
 
 popd
