db79fd22148c3251e7ba8f488140bd808e4f332b
Andre Gallant <gallantandre@gmail.com>
2023-06-01 12:38:48 -0300

    Quick cleanup of collision tests

--- blast/blast_world.hpp
+++ blast/blast_world.hpp
@@ -1,17 +1,20 @@
+#pragma once
 #include <iostream>
-#include "blast/blast.hpp"
+#include "blast.hpp"
 
-using namespace blast;
 namespace blast {
-    struct segment {
+
+const real COLLISION_EPSILON = 1e-9;
+
+struct segment {
     Vec3 p1;
     Vec3 p2;
 };
 
 struct OBB {
     Vec3 c; // OBB center point
-    Mat3 R; // Local x-, y-, and z-axes (Rotation matrix)
     Vec3 e; // Positive halfwidth extents of OBB along each axis
+    Mat3 R; // Local x-, y-, and z-axes (Rotation matrix)
 };
 
 struct surf {
@@ -52,31 +55,22 @@ struct circle {
     real r;
 };
 
-Vec3 point; 
-Vec3 Vecprin;
-Vec3 vec;
-real face_crit;
-Vec3 orgvert;
-Vec3 vert;
-real EPSILON = 1e-9;
-
 
 // A paper in 2005 suggested an elegant solution to the problem of the point in the triangle. This is this solution (adapted from https://gamedev.stackexchange.com/questions/28781/easy-way-to-project-point-onto-triangle-or-plane)
-bool pointInTriangle(Vec3 V1, Vec3 V2, Vec3 o, Vec3 P)
-{
+bool pointInTriangle(Vec3 V1, Vec3 V2, Vec3 o, Vec3 P) {
     // u=P2−P1
     Vec3 u = V1;
     // v=P3−P1
     Vec3 v = V2;
     // n=u×v
-    Vec3 n = cross(u,v);
+    Vec3 n = cross(u, v);
     // w=P−P1
     Vec3 w = P - o;
     // Barycentric coordinates of the projection P′of P onto T:
     // γ=[(u×w)⋅n]/n²
-    real gamma = dot(cross(u, w),n) / dot(n,n);
+    real gamma = dot(cross(u, w), n) / dot(n, n);
     // β=[(w×v)⋅n]/n²
-    real beta = dot(cross(w, v),n) / dot(n,n);
+    real beta = dot(cross(w, v), n) / dot(n, n);
     real alpha = 1 - gamma - beta;
     // The point P′ lies inside T if:
     return ((0 <= alpha) && (alpha <= 1) && (0 <= beta) && (beta <= 1) && (0 <= gamma) && (gamma <= 1));
@@ -85,7 +79,7 @@ bool pointInTriangle(Vec3 V1, Vec3 V2, Vec3 o, Vec3 P)
 real distmin(segment obj1, Vec3 point) {
     Vec3 ab = obj1.p2 - obj1.p1;
     real t = dot(point - obj1.p1, ab) / dot(ab, ab);
-    
+
     if (t < 0.0f) t = 0.0f;
     if (t > 1.0f) t = 1.0f;
 
@@ -106,7 +100,7 @@ Vec3 ptint(segment obj1, Vec3 point) {
 real distmin(surf surf, Vec3 point) {
     Vec3 n = cross(surf.d1, surf.d2);
 
-    if (norm(n) < EPSILON) return -INF_REAL;
+    if (norm(n) < COLLISION_EPSILON) return -INF_REAL;
 
     bool tri1 = pointInTriangle(surf.d1, surf.d2, surf.p, point);
     bool tri2 = pointInTriangle(-surf.d1, -surf.d2, surf.p + surf.d1 + surf.d2, point);
@@ -119,7 +113,7 @@ real distmin(surf surf, Vec3 point) {
         segment seg2;
         segment seg3;
         segment seg4;
-        
+
         seg1.p1 = surf.p;
         seg1.p2 = seg1.p1 + surf.d1;
         seg2.p1 = seg1.p2;
@@ -147,11 +141,11 @@ real distmin(surf surf, Vec3 point) {
 
 real distmin(segment seg1, segment seg2) {
     // From : Real-Time Detection Collision (Christer Ericson)
-    
+
     // Computes closest points C1 and C2 of S1(s)=P1+s*(Q1-P1) and
     // S2(t)=P2+t*(Q2-P2), returning s and t. Function result is squared
     // distance between between S1(s) and S2(t)
-    
+
     real s;
     real t;
     Vec3 c1;
@@ -164,14 +158,14 @@ real distmin(segment seg1, segment seg2) {
     real e = dot(d2, d2); // Squared length of segment S2, always nonnegative
     real f = dot(d2, r);
     // Check if either or both segments degenerate into points
-    if (a <= EPSILON && e <= EPSILON) {
+    if (a <= COLLISION_EPSILON && e <= COLLISION_EPSILON) {
         // Both segments degenerate into points
         s = t = 0.0f;
         c1 = seg1.p1;
         c2 = seg2.p1;
         return dot(c1 - c2, c1 - c2);
     }
-    if (a <= EPSILON) {
+    if (a <= COLLISION_EPSILON) {
         // First segment degenerates into a point
         s = 0.0f;
         t = f / e; // s = 0 => t = (b*s + f) / e = f / e
@@ -179,7 +173,7 @@ real distmin(segment seg1, segment seg2) {
     }
     else {
         real c = dot(d1, r);
-        if (e <= EPSILON) {
+        if (e <= COLLISION_EPSILON) {
             // Second segment degenerates into a point
             t = 0.0f;
             s = clamp(-c / a, 0.0f, 1.0f); // t = 0 => s = (b*t - c) / a = -c / a
@@ -214,6 +208,7 @@ real distmin(segment seg1, segment seg2) {
     c2 = seg2.p1 + d2 * t;
     return dot(c1 - c2, c1 - c2); // ATTENTION! CECI DONNE LE RESULTAT AU CARRE
 }
+
 two_pts closept(segment seg1, segment seg2) {
     // From : Real-Time Detection Collision (Christer Ericson)
 
@@ -235,7 +230,7 @@ two_pts closept(segment seg1, segment seg2) {
     real e = dot(d2, d2); // Squared length of segment S2, always nonnegative
     real f = dot(d2, r);
     // Check if either or both segments degenerate into points
-    if (a <= EPSILON && e <= EPSILON) {
+    if (a <= COLLISION_EPSILON && e <= COLLISION_EPSILON) {
         // Both segments degenerate into points
         s = t = 0.0f;
         c1 = seg1.p1;
@@ -244,7 +239,7 @@ two_pts closept(segment seg1, segment seg2) {
         result.p2 = c2;
         return result;
     }
-    if (a <= EPSILON) {
+    if (a <= COLLISION_EPSILON) {
         // First segment degenerates into a point
         s = 0.0f;
         t = f / e; // s = 0 => t = (b*s + f) / e = f / e
@@ -252,7 +247,7 @@ two_pts closept(segment seg1, segment seg2) {
     }
     else {
         real c = dot(d1, r);
-        if (e <= EPSILON) {
+        if (e <= COLLISION_EPSILON) {
             // Second segment degenerates into a point
             t = 0.0f;
             s = clamp(-c / a, 0.0f, 1.0f); // t = 0 => s = (b*t - c) / a = -c / a
@@ -290,16 +285,8 @@ two_pts closept(segment seg1, segment seg2) {
     return result;
 }
 
-Vec3 ClosestPtPointPlane(Vec3 q, plane p)
-{
+Vec3 ClosestPtPointPlane(Vec3 q, plane p) {
     return q - (dot(p.n, q - p.p) / dot(p.n, p.n)) * p.n;;
-}
-
-int signx(real x) {
-    if (x < 0) {
-        return -1;
-    }
-    return 1;
 }
 
 two_pts intersection(circle circ, segment seg) {
@@ -308,7 +295,7 @@ two_pts intersection(circle circ, segment seg) {
     real dr_sq = dx * dx + dy * dy;
     real D = seg.p1.x * seg.p2.y - seg.p2.x * seg.p1.y;
 
-    real det = sqrt(circ.r * circ.r * dr_sq - D * D);
+    real det = (circ.r * circ.r * dr_sq - D * D);
 
     if (det < 0) {
         Vec3 point = ptint(seg, circ.p);
@@ -316,14 +303,16 @@ two_pts intersection(circle circ, segment seg) {
         return { pointcirc, pointcirc };
     }
 
-    real x1 = (1 / dr_sq) * (D * dy + signx(dy) * dx * det);
-    real x2 = (1 / dr_sq) * (D * dy - signx(dy) * dx * det);
+    det = sqrt(det); // note: only sqrt if det positive
+    real sign_dy = dy > 0 ? 1 : -1;
+    real x1 = (1 / dr_sq) * (D * dy + sign_dy * dx * det);
+    real x2 = (1 / dr_sq) * (D * dy - sign_dy * dx * det);
 
     real y1 = (1 / dr_sq) * (-D * dx + abs(dy) * det);
     real y2 = (1 / dr_sq) * (-D * dx - abs(dy) * det);
 
-    Vec3 p1 = { x1,y1,0 };
-    Vec3 p2 = { x2,y2,0 };
+    Vec3 p1 = { x1, y1, 0 };
+    Vec3 p2 = { x2, y2, 0 };
     return { p1, p2 };
 }
 
@@ -334,19 +323,19 @@ real distmin(capsule caps, cylinder cyl) {
     seg1.p2 = caps.p2;
     seg2.p1 = cyl.p1;
     seg2.p2 = cyl.p2;
-    
+
     two_pts points = closept(seg1, seg2); // !! Erreur dans cette formule !!
     real cond1 = dot(points.p2 - cyl.p1, points.p2 - cyl.p1);
     real cond2 = dot(points.p2 - cyl.p2, points.p2 - cyl.p2);
 
     // Depending on which point of the cylinder is closest, we will change the face which we check
-    if (cond1 < EPSILON || cond2 < EPSILON) {
+    if (cond1 < COLLISION_EPSILON || cond2 < COLLISION_EPSILON) {
         Vec3 cent;
         Vec3 other;
 
         cent = cyl.p1;
         other = cyl.p2;
-        
+
         if (cond2 < cond1) {
             cent = cyl.p2;
             other = cyl.p1;
@@ -368,14 +357,12 @@ real distmin(capsule caps, cylinder cyl) {
 
         real dist_sq;
         dist_sq = rad_sq1;
-        if (rad_sq2 < rad_sq1) {
+        if (rad_sq2 < rad_sq1)
             dist_sq = rad_sq2;
-        }
 
         // if both points project on the circle plane, then the distance will be the minimum of the two normal distances calculated
-        if (rad_sq1 < cyl.r * cyl.r && rad_sq2 < cyl.r * cyl.r) {
+        if (rad_sq1 < cyl.r * cyl.r && rad_sq2 < cyl.r * cyl.r)
             return sqrt(dist_sq) - caps.r;
-        }
 
         // project the points on the plan
         segment proj;
@@ -390,17 +377,16 @@ real distmin(capsule caps, cylinder cyl) {
         real dist1 = distmin(seg1, pts.p1);
         real dist2 = distmin(seg1, pts.p2);
 
-        if (dist2 < dist1 && dist2*dist2 < dist_sq) {
+        if (dist2 < dist1 && dist2*dist2 < dist_sq)
             return dist2 - caps.r;
-        }
-        if (dist1 < dist2 && dist1 * dist1 < dist_sq) {
+
+        if (dist1 < dist2 && dist1 * dist1 < dist_sq)
             return dist1 - caps.r;
-        }
+
         return sqrt(dist_sq) - caps.r;
     }
-    else {
+    else
         return norm(points.p1 - points.p2) - caps.r - cyl.r;
-    }
 }
 
 real distmin(capsule caps, sphere sph) {
@@ -423,12 +409,11 @@ real distmin(capsule caps1, capsule caps2) {
 }
 
 real distmin(OBB OBB, segment seg) {
-    Vecprin = seg.p2 - seg.p1;
 
     Mat3 Rtrans = transpose(OBB.R);
 
-    vec = Rtrans * (seg.p2 - seg.p1);
-    point = Rtrans * (seg.p2 - OBB.c);
+    Vec3 vec = Rtrans * (seg.p2 - seg.p1);
+    Vec3 point = Rtrans * (seg.p2 - OBB.c);
 
     if (vec.z < 0) {
         vec = -vec;
@@ -441,18 +426,18 @@ real distmin(OBB OBB, segment seg) {
     real ymax = + OBB.e[1];
     real zmin = - OBB.e[2];
     real zmax = + OBB.e[2];
-    
+
     // Creating the eight original vertices
     Vec3 orgvert[8];
 
-    orgvert[0] = { xmin,ymin,zmin };
-    orgvert[1] = { xmin,ymax,zmin };
-    orgvert[2] = { xmax,ymin,zmin };
-    orgvert[3] = { xmax,ymax,zmin };
-    orgvert[4] = { xmin,ymin,zmax };
-    orgvert[5] = { xmin,ymax,zmax };
-    orgvert[6] = { xmax,ymin,zmax };
-    orgvert[7] = { xmax,ymax,zmax };
+    orgvert[0] = { xmin, ymin, zmin };
+    orgvert[1] = { xmin, ymax, zmin };
+    orgvert[2] = { xmax, ymin, zmin };
+    orgvert[3] = { xmax, ymax, zmin };
+    orgvert[4] = { xmin, ymin, zmax };
+    orgvert[5] = { xmin, ymax, zmax };
+    orgvert[6] = { xmax, ymin, zmax };
+    orgvert[7] = { xmax, ymax, zmax };
 
     // Thus, we get the three main directions
     Vec3 dir[3];
@@ -475,7 +460,7 @@ real distmin(OBB OBB, segment seg) {
     vert[5] = orgvert[5] + vec;
     vert[6] = orgvert[6] + vec;
     vert[7] = orgvert[7] + vec;
-    
+
     // some faces depend only on the direction of vec in x
     surf face[12];
 
@@ -571,13 +556,13 @@ real distmin(OBB OBB, segment seg) {
         face[10].d2 = vec;
 
         face[11].p = orgvert[6];
-        face[11].d1 = -dir[2]; 
+        face[11].d1 = -dir[2];
         face[11].d2 = vec;
     }
 
     if (vec.x >= 0 && vec.y < 0) {
         face[10].p = orgvert[4];
-        face[10].d1 = -dir[2]; 
+        face[10].d1 = -dir[2];
         face[10].d2 = vec;
 
         face[11].p = orgvert[3];
@@ -606,78 +591,120 @@ real distmin(OBB OBB, segment seg) {
     }
 
     real dist[12];
-    
-    real distmax = -INFINITY;
+
+    real distmax = -INF_REAL;
 
     for (int i = 0; i < 12; i++) {
         dist[i] = distmin(face[i], point);
         // std::cout << "La distance de la face " << i << " est " << dist[i] << std::endl;
-        if (dist[i] > distmax) {
+        if (dist[i] > distmax)
             distmax = dist[i];
-            face_crit = i;
-        }
     }
     return distmax;
 }
 
+} // namespace blast
+
+
+
+#ifdef BLAST_ENABLE_TESTS
+TEST_CASE("OBB collision", "[World]") {
+    using namespace blast;
+
+    struct collision_test {
+        OBB box;
+        segment seg;
+        real expected_dist;
+    };
+
+    collision_test test[] = {
+        { { { 0, 0, 0 }, { 1, 2, 3 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+        { { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+        { { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+        { { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+        { { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+        { { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+        { { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+        { { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+        { { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+        { { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+        { { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+        { { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1 } }, { { 0, 0, 0 }, { 1, 1, 1 } }, 0 },
+    };
+
+    for (auto t : test) {
+        real dist = distmin(t.box, t.seg);
+        CHECK(dist == t.expected_dist);
+    }
 }
 
-int main()
-{
-    // Initialisation de l'OBB test
-    OBB OBBTEST;
-    OBBTEST.c = { 0, 0, 0 };
-    OBBTEST.e = { 1, 2, 3 };
-    OBBTEST.R = { 1,0,0, 0,1,0, 0,0,1 };
-
-    // Initialisation du segment test
-    segment SEGMENTTEST;
-    SEGMENTTEST.p1 = { 3,3,0.5 };
-    SEGMENTTEST.p2 = { 2,2,2 };
-
-    real rayon = 0;
-    real dist = distmin(OBBTEST, SEGMENTTEST) - rayon;
-    std::cout << "Le vecteur est defini dans les axes principaux par un point en " << SEGMENTTEST.p2[0] << ", " << SEGMENTTEST.p2[1] << ", " << SEGMENTTEST.p2[2] << " et un vecteur aux valeurs " << Vecprin[0] << ", " << Vecprin[1] << ", " << Vecprin[2] << std::endl;
-    std::cout << "Le vecteur est defini dans les axes de l OBB par un point en " << point[0] << ", " << point[1] << ", " << point[2] << " et un vecteur aux valeurs (signes inverses) " << vec[0] << ", " << vec[1] << ", " << vec[2] << std::endl;
-    std::cout << "La distance maximale entre les faces de l OBB et le segment est de " << dist << " et celle-ci est avec la face " << face_crit << std::endl;
-
-    // FORMAT TEST
-    //// Initialisation de l'OBB test
-    //OBB OBBTEST;
-    //OBBTEST.c = { 0, 0, 0 };
-    //OBBTEST.e = { 1, 2, 3 };
-    //OBBTEST.R = { 1,0,0, 0,1,0, 0,0,1 };
-    //
-    //// Initialisation du segment test
-    // Vec3 size = transpose(OBBTEST.R)*OBBTEST.e;
-    // Vec3 q1 = { -1.2, -1.1, -1 };
-    // Vec3 q2 = { -1.9, -1.5, -2 };
-    // real rayon = 0;
-    // 
-    // segment SEGMENTTEST;
-    // SEGMENTTEST.p1 = OBBTEST.c +q1.x*size.x + q1.y*size.y + q1.z*size.z;
-    // SEGMENTTEST.p2 = OBBTEST.c +q2.x*size.x + q2.y*size.y + q2.z*size.z;
-    // 
-    // real dist = distmin(OBBTEST, SEGMENTTEST) - rayon;
-
-    //// Test du programme (point-plan)
-    //surf SURFTEST;
-    //SURFTEST.d1 = { 1,0,0 };
-    //SURFTEST.d2 = { 0,1,0 };
-    //SURFTEST.p = { 0,0,0 };
-
-    //Vec3 POINTTEST = { 2,2,0 };
-    //real dist = distmin(SURFTEST, POINTTEST);
-
-    //std::cout << "La distance entre le point et le plan est " << dist << std::endl;
-
-    //// Test de Triangle point
-    //Vec3 V1 = { 0,1,1 };
-    //Vec3 V2 = { 1,0,1 };
-    //Vec3 o = { 0,0,0 };
-    //Vec3 P = { 1,1,1 };
-    //
-    //bool point = pointInTriangle(V1, V2, o, P);
-
-    //std::cout << point;
+TEST_CASE("Sphere collision", "[World]") {
 }
+
+TEST_CASE("Cylinder collision", "[World]") {
+
+}
+
+TEST_CASE("Capsule collision", "[World]") {
+}
+
+#endif
+
+// int main() {
+//     // Initialisation de l'OBB test
+//     OBB OBBTEST;
+//     OBBTEST.c = { 0, 0, 0 };
+//     OBBTEST.e = { 1, 2, 3 };
+//     OBBTEST.R = { 1, 0, 0, 0, 1, 0, 0, 0, 1 };
+
+//     // Initialisation du segment test
+//     segment SEGMENTTEST;
+//     SEGMENTTEST.p1 = { 3, 3, 0.5 };
+//     SEGMENTTEST.p2 = { 2, 2, 2 };
+
+//     real rayon = 0;
+//     real dist = distmin(OBBTEST, SEGMENTTEST) - rayon;
+//     std::cout << "Le vecteur est defini dans les axes principaux par un point en " << SEGMENTTEST.p2[0] << ", " << SEGMENTTEST.p2[1] << ", " << SEGMENTTEST.p2[2] << " et un vecteur aux valeurs " << Vecprin[0] << ", " << Vecprin[1] << ", " << Vecprin[2] << std::endl;
+//     std::cout << "Le vecteur est defini dans les axes de l OBB par un point en " << point[0] << ", " << point[1] << ", " << point[2] << " et un vecteur aux valeurs (signes inverses) " << vec[0] << ", " << vec[1] << ", " << vec[2] << std::endl;
+//     std::cout << "La distance maximale entre les faces de l OBB et le segment est de " << dist << " et celle-ci est avec la face " << face_crit << std::endl;
+
+//     // FORMAT TEST
+//     //// Initialisation de l'OBB test
+//     //OBB OBBTEST;
+//     //OBBTEST.c = { 0, 0, 0 };
+//     //OBBTEST.e = { 1, 2, 3 };
+//     //OBBTEST.R = { 1,0,0, 0,1,0, 0,0,1 };
+//     //
+//     //// Initialisation du segment test
+//     // Vec3 size = transpose(OBBTEST.R)*OBBTEST.e;
+//     // Vec3 q1 = { -1.2, -1.1, -1 };
+//     // Vec3 q2 = { -1.9, -1.5, -2 };
+//     // real rayon = 0;
+//     //
+//     // segment SEGMENTTEST;
+//     // SEGMENTTEST.p1 = OBBTEST.c +q1.x*size.x + q1.y*size.y + q1.z*size.z;
+//     // SEGMENTTEST.p2 = OBBTEST.c +q2.x*size.x + q2.y*size.y + q2.z*size.z;
+//     //
+//     // real dist = distmin(OBBTEST, SEGMENTTEST) - rayon;
+
+//     //// Test du programme (point-plan)
+//     //surf SURFTEST;
+//     //SURFTEST.d1 = { 1,0,0 };
+//     //SURFTEST.d2 = { 0,1,0 };
+//     //SURFTEST.p = { 0,0,0 };
+
+//     //Vec3 POINTTEST = { 2,2,0 };
+//     //real dist = distmin(SURFTEST, POINTTEST);
+
+//     //std::cout << "La distance entre le point et le plan est " << dist << std::endl;
+
+//     //// Test de Triangle point
+//     //Vec3 V1 = { 0,1,1 };
+//     //Vec3 V2 = { 1,0,1 };
+//     //Vec3 o = { 0,0,0 };
+//     //Vec3 P = { 1,1,1 };
+//     //
+//     //bool point = pointInTriangle(V1, V2, o, P);
+
+//     //std::cout << point;
+// }
