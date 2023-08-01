24991aa0eea8ba9b22722edb784d3cdf65af00f1
Andre Gallant <gallantandre@gmail.com>
2023-08-01 17:03:53 -0300

    Add stashed benchmarks

--- build_tests.bat
+++ build_tests.bat
@@ -5,7 +5,7 @@ pushd "examples/build"
 
 set includes=-I "../../blast/" -I "../../blast/optional"
 set options= --std c++17 -m64 -O0 -g -D BLAST_DEBUG
-@REM set options= --std c++17 -m64 -O3
+@REM set options= --std c++17 -m64 -O3 -Xcompiler /arch:AVX512
 nvcc ../tests.cu -o tests.exe %includes% %options% %libr%
 
 popd
