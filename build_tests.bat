d06f7e84e4c86f4146ddaeadb9d10781143541ef
Andre Gallant <gallantandre@umoncton.ca>
2023-11-07 19:17:29 -0400

    Move benchmarks to main

--- build_tests.bat
+++ build_tests.bat
@@ -4,8 +4,8 @@ echo 'building tests and benchmarks'
 pushd "examples/build"
 
 set includes=-I "../../blast/" -I "../../blast/optional"
-@REM set options= --std c++17 -m64 -O0 -g -D BLAST_DEBUG -Xcompiler "-std:c++17 -Od /EHsc /MT /Z7 /arch:AVX2"
-set options= --std c++17 -m64 -O2 -Xcompiler "-std:c++17 -O2 /EHsc /MT /Z7 /arch:AVX2"
+set options= --std c++17 -m64 -O0 -g -D BLAST_DEBUG -Xcompiler "-std:c++17 -Od /EHsc /MT /Zi /arch:AVX2"
+@REM set options= --std c++17 -m64 -O2 -Xcompiler "-std:c++17 -O2 /EHsc /MT /Z7 /arch:AVX2"
 nvcc ../tests.cu -o tests.exe %includes% %options% %libr%
 
 popd
--- examples/tests.cu
+++ examples/tests.cu
@@ -7,6 +7,7 @@
 
 
 // Benchmarks
+
 // TEST_CASE("GPU trajectory computation speed", "[Trajectory]") {
 //     using namespace blast;
 //     const u32 points = 256;
@@ -49,6 +50,52 @@
 //             gpu_bsplines.compute_trajectories(x_multi, task);
 //             gpu_bsplines.fetch_trajectories();
 //             return gpu_bsplines.pos[0];
+//         };
+//     }
+// }
+
+// TEST_CASE("GPU dynamics computation speed", "[Manipulator]") {
+//     using namespace blast;
+//     const u32 points = 256;
+//     const u32 joints = 7;
+//     const u32 p = 5;
+//     const u32 ncontrol = 24;
+//     const u32 ntrajectories = 80;
+//     cuMultiBsplines dev_bsplines(points, joints, p, ncontrol, ntrajectories);
+//     Bspline host_bspline(ncontrol, points, p, joints);
+//     // random task
+//     Matrix task(joints, 6);
+//     real amp = 10;
+//     for (u32 i = 0; i < task.rows; i++)
+//         for (u32 j = 0; j < task.cols; j++)
+//             task(i, j) = amp * get_random();
+//     // random optimization vector
+//     Array x(joints*(ncontrol-6) + 1);
+//     for (u32 i = 0; i < x.size; i++)
+//         x[i] = amp * get_random();
+//     x.back() = abs(x.back());
+//     // copy the 'x' Array 'ntrajectories' times for the gpu version
+//     const u32 xlen = host_bspline.xlen(task);
+//     Array x_multi(ntrajectories * host_bspline.xlen(task));
+//     for (int i = 0; i < ntrajectories; i++) {
+//         for (int j = 0; j < xlen; j++) {
+//             x_multi[i*xlen + j] = x[j];
+//         }
+//     }
+//     {
+//         std::string msg = "Computing " + std::to_string(ntrajectories) + " trajectories on the CPU";
+//         BENCHMARK(msg.c_str()) {
+//             for (int i = 0; i < ntrajectories; i++)
+//                 host_bspline.compute_trajectory(x, task);
+//             return host_bspline.traj.pos(0, 0);
+//         };
+//     }
+//     {
+//         std::string msg = "Computing " + std::to_string(ntrajectories) + " trajectories on the GPU";
+//         BENCHMARK(msg.c_str()) {
+//             dev_bsplines.compute_trajectories(x_multi, task);
+//             dev_bsplines.fetch_trajectories();
+//             return dev_bsplines.pos[0];
 //         };
 //     }
 // }
