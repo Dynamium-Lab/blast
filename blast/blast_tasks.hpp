0ac4e97c1fba9dd27d98973563e9cbbe33fd884a
ThomasDuquette <133025159+ThomasDuquette@users.noreply.github.com>
2024-02-22 09:27:07 -0400

    Set acceleration to 0 for all joints in order to have a smooth transition

--- blast/blast_tasks.hpp
+++ blast/blast_tasks.hpp
@@ -53,10 +53,11 @@ std::list<Manipulator_state> Link6::pick_and_place(const Manipulator_state &Curr
     Array joint_vel2(N_joints);
     Array joint_vel3(N_joints);
     Array joint_vel4(N_joints);
-    Array joint_acc1(N_joints);
-    Array joint_acc2(N_joints);
-    Array joint_acc3(N_joints);
-    Array joint_acc4(N_joints);
+    // Array joint_acc1(N_joints);
+    // Array joint_acc2(N_joints);
+    // Array joint_acc3(N_joints);
+    // Array joint_acc4(N_joints);
+    Array joint_acc(N_joints, 0);
 
     for (int i = 0; i < N_objects; i++) {
         // Joint positions
@@ -79,58 +80,67 @@ std::list<Manipulator_state> Link6::pick_and_place(const Manipulator_state &Curr
         joint_vel1 = pinv(jacobian(joint_pos1))*v;
         joint_vel3 = pinv(jacobian(joint_pos3))*v;
 
-        // Joint acceleration
-        for (int j = 0; j < N_joints; j++) {
-            joint_acc2[j] = 0;
-            joint_acc4[j] = 0;
-        }
-        joint_acc1 = pinv(jacobian(joint_pos1))*a;
-        joint_acc3 = pinv(jacobian(joint_pos3))*a;
+        // todo: If we use joint acceleration, we need to change the formula for 1 and 3 since it is not right
+        // // Joint acceleration
+        // for (int j = 0; j < N_joints; j++) {
+        //     joint_acc2[j] = 0;
+        //     joint_acc4[j] = 0;
+        // }
+        // joint_acc1 = pinv(jacobian(joint_pos1))*a;
+        // joint_acc3 = pinv(jacobian(joint_pos3))*a;
 
         // Filling manip list
-        manip_state.PVA = add_arrays(add_arrays(joint_pos1, -joint_vel1), -joint_acc1);
+        // manip_state.PVA = add_arrays(add_arrays(joint_pos1, -joint_vel1), -joint_acc1);
+        manip_state.PVA = add_arrays(add_arrays(joint_pos1, -joint_vel1), joint_acc);
         manip_state.gripper_state = 0;
         manip_state.mvt_robot;
         manip_state.traj_gen_blast;
         manip_list.push_back(manip_state);
 
-        manip_state.PVA = add_arrays(add_arrays(joint_pos2, joint_vel2), joint_acc2);
+        // manip_state.PVA = add_arrays(add_arrays(joint_pos2, joint_vel2), joint_acc2);
+        manip_state.PVA = add_arrays(add_arrays(joint_pos2, joint_vel2), joint_acc);
         manip_state.gripper_state = 0;
         manip_state.mvt_robot;
         manip_state.traj_gen_straight;
         manip_list.push_back(manip_state);
 
-        manip_state.PVA = add_arrays(add_arrays(joint_pos2, joint_vel2), joint_acc2);
+        // manip_state.PVA = add_arrays(add_arrays(joint_pos2, joint_vel2), joint_acc2);
+        manip_state.PVA = add_arrays(add_arrays(joint_pos2, joint_vel2), joint_acc);
         manip_state.gripper_state = 1;
         manip_state.mvt_gripper;
         manip_state.traj_gen_null;
         manip_list.push_back(manip_state);
 
-        manip_state.PVA = add_arrays(add_arrays(joint_pos1, joint_vel1), joint_acc1);
+        // manip_state.PVA = add_arrays(add_arrays(joint_pos1, joint_vel1), joint_acc1);
+        manip_state.PVA = add_arrays(add_arrays(joint_pos1, joint_vel1), joint_acc);
         manip_state.gripper_state = 0;
         manip_state.mvt_robot;
         manip_state.traj_gen_straight;
         manip_list.push_back(manip_state);
 
-        manip_state.PVA = add_arrays(add_arrays(joint_pos3, -joint_vel3), -joint_acc3);
+        // manip_state.PVA = add_arrays(add_arrays(joint_pos3, -joint_vel3), -joint_acc3);
+        manip_state.PVA = add_arrays(add_arrays(joint_pos3, -joint_vel3), joint_acc);
         manip_state.gripper_state = 0;
         manip_state.mvt_robot;
         manip_state.traj_gen_blast;
         manip_list.push_back(manip_state);
 
-        manip_state.PVA = add_arrays(add_arrays(joint_pos4, joint_vel4), joint_acc4);
+        // manip_state.PVA = add_arrays(add_arrays(joint_pos4, joint_vel4), joint_acc4);
+        manip_state.PVA = add_arrays(add_arrays(joint_pos4, joint_vel4), joint_acc);
         manip_state.gripper_state = 0;
         manip_state.mvt_robot;
         manip_state.traj_gen_straight;
         manip_list.push_back(manip_state);
 
-        manip_state.PVA = add_arrays(add_arrays(joint_pos4, joint_vel4), joint_acc4);
+        // manip_state.PVA = add_arrays(add_arrays(joint_pos4, joint_vel4), joint_acc4);
+        manip_state.PVA = add_arrays(add_arrays(joint_pos4, joint_vel4), joint_acc);
         manip_state.gripper_state = 1;
         manip_state.mvt_gripper;
         manip_state.traj_gen_null;
         manip_list.push_back(manip_state);
 
-        manip_state.PVA = add_arrays(add_arrays(joint_pos3, joint_vel3), joint_acc3);
+        // manip_state.PVA = add_arrays(add_arrays(joint_pos3, joint_vel3), joint_acc3);
+        manip_state.PVA = add_arrays(add_arrays(joint_pos3, joint_vel3), joint_acc);
         manip_state.gripper_state = 0;
         manip_state.mvt_robot;
         manip_state.traj_gen_straight;
