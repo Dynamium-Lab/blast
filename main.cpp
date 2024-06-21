95fc92993d4921e60efd30f80c95328a09d3920d
Andre Gallant <gallantandre@umoncton.ca>
2024-06-20 21:29:48 -0300

    Add more information in profiler

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
+#define PROFILER 1
 #include "blast.h"
 #include "blast_utilities.h"
 #include <iostream>
 
 
-
 int main() {
+    blast::BeginProfile();
     using blast::print;
     using std::cout;
     using std::endl;
-
-
-
+    {
+        TimeBlock("Main");
+        {
+            TimeBlock("Sleep");
+            Sleep(1000);
+        }
+    }
+    blast::EndAndPrintProfile();
     return 0;
 }
+ProfilerEndOfCompilationUnit;
