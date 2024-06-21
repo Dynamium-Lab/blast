a67526c66d4cc7a9fd883ac9b43998fa1072f8b7
Andre Gallant <gallantandre@umoncton.ca>
2024-06-21 09:44:36 -0300

    Cleanup profiler

--- src/blast_profile.hpp
+++ src/blast_profile.hpp
@@ -7,74 +7,41 @@
 #include <Windows.h>
 #undef NOMINMAX
 #undef WIN32_LEAN_AND_MEAN
+#else
+#include <x86intrin.h>
+#include <sys/time.h>
 #endif
 
 namespace blast {
-#define ArrayCount(Array) (sizeof(Array)/sizeof((Array)[0]))
+#define array_count(array) (sizeof(array)/sizeof((array)[0]))
 
 
 #if _WIN32
-
-static u64 GetOSTimerFreq(void) {
-    LARGE_INTEGER Freq;
-    QueryPerformanceFrequency(&Freq);
-    return Freq.QuadPart;
+static u64 get_os_freq(void) {
+    LARGE_INTEGER freq;
+    QueryPerformanceFrequency(&freq);
+    return freq.QuadPart;
 }
-
-static u64 ReadOSTimer(void) {
-    LARGE_INTEGER Value;
-    QueryPerformanceCounter(&Value);
-    return Value.QuadPart;
+static u64 read_os_timer(void) {
+    LARGE_INTEGER time;
+    QueryPerformanceCounter(&time);
+    return time.QuadPart;
 }
 
 #else
-
-#include <x86intrin.h>
-#include <sys/time.h>
-
-static u64 GetOSTimerFreq(void) {
+static u64 get_os_freq(void) {
     return 1000000;
 }
-
-static u64 ReadOSTimer(void) {
-    // NOTE(casey): The "struct" keyword is not necessary here when compiling in C++,
-    // but just in case anyone is using this file from C, I include it.
-    struct timeval Value;
-    gettimeofday(&Value, 0);
-
-    u64 Result = GetOSTimerFreq()*(u64)Value.tv_sec + (u64)Value.tv_usec;
-    return Result;
+static u64 read_os_timer(void) {
+    timeval time;
+    gettimeofday(&time, 0);
+    u64 result = get_os_freq()*(u64)time.tv_sec + (u64)time.tv_usec;
+    return result;
 }
-
 #endif
 
-inline u64 ReadCPUTimer(void) {
+inline u64 read_cpu_timer(void) {
     return __rdtsc();
-}
-
-static u64 EstimateCPUTimerFreq(void) {
-    u64 MillisecondsToWait = 100;
-    u64 OSFreq = GetOSTimerFreq();
-
-    u64 CPUStart = ReadCPUTimer();
-    u64 OSStart = ReadOSTimer();
-    u64 OSEnd = 0;
-    u64 OSElapsed = 0;
-    u64 OSWaitTime = OSFreq * MillisecondsToWait / 1000;
-    while(OSElapsed < OSWaitTime) {
-        OSEnd = ReadOSTimer();
-        OSElapsed = OSEnd - OSStart;
-    }
-
-    u64 CPUEnd = ReadCPUTimer();
-    u64 CPUElapsed = CPUEnd - CPUStart;
-
-    u64 CPUFreq = 0;
-    if(OSElapsed) {
-        CPUFreq = OSFreq * CPUElapsed / OSElapsed;
-    }
-
-    return CPUFreq;
 }
 
 #ifndef PROFILER
@@ -82,159 +49,152 @@ static u64 EstimateCPUTimerFreq(void) {
 #endif
 
 #ifndef READ_BLOCK_TIMER
-#define READ_BLOCK_TIMER ReadCPUTimer
+#define READ_BLOCK_TIMER read_cpu_timer
 #endif
 
 #if PROFILER
 
-struct profile_anchor {
-    u64 TSCElapsedExclusive;
-    u64 TSCElapsedInclusive;
-    u64 HitCount;
-    u64 ProcessedByteCount;
-    char const *Label;
+struct ProfileAnchor {
+    u64 TSC_elapsed_exclusive;
+    u64 TSC_elapsed_inclusive;
+    u64 hit_count;
+    u64 byte_count;
+    char const *label;
 };
-static profile_anchor GlobalProfilerAnchors[4096];
-static u32 GlobalProfilerParent;
+static ProfileAnchor g_profiler_anchors[4096];
+static u32 g_profiler_parent;
 
-struct profile_block {
-    profile_block(char const *Label_, u32 AnchorIndex_, u64 ByteCount) {
-        ParentIndex = GlobalProfilerParent;
+struct ProfileBlock {
+    ProfileBlock(char const *label_, u32 anchor_id_, u64 byte_count) {
+        parent_id = g_profiler_parent;
 
-        AnchorIndex = AnchorIndex_;
-        Label = Label_;
+        anchor_id = anchor_id_;
+        label = label_;
 
-        profile_anchor *Anchor = GlobalProfilerAnchors + AnchorIndex;
-        OldTSCElapsedInclusive = Anchor->TSCElapsedInclusive;
-        Anchor->ProcessedByteCount += ByteCount;
+        ProfileAnchor *anchor = g_profiler_anchors + anchor_id;
+        old_TSC_elapsed_inclusive = anchor->TSC_elapsed_inclusive;
+        anchor->byte_count += byte_count;
 
-        GlobalProfilerParent = AnchorIndex;
-        StartTSC = READ_BLOCK_TIMER();
+        g_profiler_parent = anchor_id;
+        start_TSC = READ_BLOCK_TIMER();
     }
 
-    ~profile_block(void) {
-        u64 Elapsed = READ_BLOCK_TIMER() - StartTSC;
-        GlobalProfilerParent = ParentIndex;
+    ~ProfileBlock(void) {
+        u64 elapsed = READ_BLOCK_TIMER() - start_TSC;
+        g_profiler_parent = parent_id;
 
-        profile_anchor *Parent = GlobalProfilerAnchors + ParentIndex;
-        profile_anchor *Anchor = GlobalProfilerAnchors + AnchorIndex;
+        ProfileAnchor *parent = g_profiler_anchors + parent_id;
+        ProfileAnchor *anchor = g_profiler_anchors + anchor_id;
 
-        Parent->TSCElapsedExclusive -= Elapsed;
-        Anchor->TSCElapsedExclusive += Elapsed;
-        Anchor->TSCElapsedInclusive = OldTSCElapsedInclusive + Elapsed;
-        ++Anchor->HitCount;
-
-        /* NOTE(casey): This write happens every time solely because there is no
-           straightforward way in C++ to have the same ease-of-use. In a better programming
-           language, it would be simple to have the anchor points gathered and labeled at compile
-           time, and this repetative write would be eliminated. */
-        Anchor->Label = Label;
+        parent->TSC_elapsed_exclusive -= elapsed;
+        anchor->TSC_elapsed_exclusive += elapsed;
+        anchor->TSC_elapsed_inclusive = old_TSC_elapsed_inclusive + elapsed;
+        anchor->hit_count++;
+        anchor->label = label;
     }
 
-    char const *Label;
-    u64 OldTSCElapsedInclusive;
-    u64 StartTSC;
-    u32 ParentIndex;
-    u32 AnchorIndex;
+    char const *label;
+    u64 old_TSC_elapsed_inclusive;
+    u64 start_TSC;
+    u32 parent_id;
+    u32 anchor_id;
 };
 
 #define NameConcat2(A, B) A##B
 #define NameConcat(A, B) NameConcat2(A, B)
-#define TimeBandwidth(Name, bytes) blast::profile_block NameConcat(Block, __LINE__)(Name, __COUNTER__ + 1, bytes);
-#define ProfilerEndOfCompilationUnit static_assert(__COUNTER__ < ArrayCount(blast::GlobalProfilerAnchors), "Number of profile points exceeds size of profiler::Anchors array")
+#define blast_time_bandwidth(Name, bytes) blast::ProfileBlock NameConcat(Block, __LINE__)(Name, __COUNTER__ + 1, bytes);
+#define blast_profiler_end_compilation_unit static_assert(__COUNTER__ < array_count(blast::g_profiler_anchors), "Number of profile points exceeds size of profiler::anchors array")
 
-static void PrintTimeElapsed(u64 TotalTSCElapsed, u64 TimerFreq, profile_anchor *Anchor) {
-    double Percent = 100.0 * ((double)Anchor->TSCElapsedExclusive / (double)TotalTSCElapsed);
-    printf("  %s[%llu]: %llu (%.2f%%", Anchor->Label, Anchor->HitCount, Anchor->TSCElapsedExclusive, Percent);
-    if(Anchor->TSCElapsedInclusive != Anchor->TSCElapsedExclusive) {
-        double PercentWithChildren = 100.0 * ((double)Anchor->TSCElapsedInclusive / (double)TotalTSCElapsed);
-        printf(", %.2f%% w/children", PercentWithChildren);
+static void print_time_elapsed(u64 total_elapsed, u64 freq, ProfileAnchor *anchor) {
+    double percent = 100.0 * ((double)anchor->TSC_elapsed_exclusive / (double)total_elapsed);
+    printf("  %s[%llu]: %llu (%.2f%%", anchor->label, anchor->hit_count, anchor->TSC_elapsed_exclusive, percent);
+    if(anchor->TSC_elapsed_inclusive != anchor->TSC_elapsed_exclusive) {
+        double percent_w_children = 100.0 * ((double)anchor->TSC_elapsed_inclusive / (double)total_elapsed);
+        printf(", %.2f%% w/children", percent_w_children);
     }
     printf(")");
 
-    if(Anchor->ProcessedByteCount) {
-        double Megabyte = 1024.0f*1024.0f;
-        double Gigabyte = Megabyte*1024.0f;
+    if(anchor->byte_count) {
+        double MB = 1024.0f*1024.0f;
+        double GB = MB*1024.0f;
 
-        double Seconds = (double)Anchor->TSCElapsedInclusive / (double)TimerFreq;
-        double BytesPerSecond = (double)Anchor->ProcessedByteCount / Seconds;
-        double Megabytes = (double)Anchor->ProcessedByteCount / (double)Megabyte;
-        double GigabytesPerSecond = BytesPerSecond / Gigabyte;
+        double seconds = (double)anchor->TSC_elapsed_inclusive / (double)freq;
+        double bytes_per_second = (double)anchor->byte_count / seconds;
+        double megabytes = (double)anchor->byte_count / (double)MB;
+        double GB_per_second = bytes_per_second / GB;
 
-        printf("  %.3fmb at %.2fgb/s", Megabytes, GigabytesPerSecond);
+        printf("  %.3fmb at %.2fgb/s", megabytes, GB_per_second);
     }
 
     printf("\n");
 }
 
-static void PrintAnchorData(u64 TotalCPUElapsed, u64 TimerFreq) {
-    for(u32 AnchorIndex = 0; AnchorIndex < ArrayCount(GlobalProfilerAnchors); ++AnchorIndex) {
-        profile_anchor *Anchor = GlobalProfilerAnchors + AnchorIndex;
-        if(Anchor->TSCElapsedInclusive) {
-            PrintTimeElapsed(TotalCPUElapsed, TimerFreq, Anchor);
+static void print_anchor(u64 total_CPU_elapsed, u64 freq) {
+    for(u32 anchor_id = 0; anchor_id < array_count(g_profiler_anchors); ++anchor_id) {
+        ProfileAnchor *anchor = g_profiler_anchors + anchor_id;
+        if(anchor->TSC_elapsed_inclusive) {
+            print_time_elapsed(total_CPU_elapsed, freq, anchor);
         }
     }
 }
 
 #else
 
-#define TimeBandwidth(...)
-#define PrintAnchorData(...)
-#define ProfilerEndOfCompilationUnit
+#define blast_time_bandwidth(...)
+#define print_anchor(...)
+#define blast_profiler_end_compilation_unit
 
 #endif
 
-struct profiler {
-    u64 StartTSC;
-    u64 EndTSC;
+struct Profiler {
+    u64 start_TSC;
+    u64 end_TSC;
 };
-static profiler GlobalProfiler;
+static Profiler g_profiler;
 
-#define TimeBlock(Name) TimeBandwidth(Name, 0)
-#define TimeFunction TimeBlock(__func__)
+#define blast_time_block(Name) blast_time_bandwidth(Name, 0)
+#define blast_time_function blast_time_block(__func__)
 
-static u64 EstimateBlockTimerFreq(void) {
-    (void)&EstimateCPUTimerFreq; // NOTE(casey): This has to be voided here to prevent compilers from warning us that it is not used
+static u64 estimate_block_freq(void) {
+    u64 ms_to_wait = 100;
+    u64 os_freq = get_os_freq();
 
-    u64 MillisecondsToWait = 100;
-    u64 OSFreq = GetOSTimerFreq();
-
-    u64 BlockStart = READ_BLOCK_TIMER();
-    u64 OSStart = ReadOSTimer();
-    u64 OSEnd = 0;
-    u64 OSElapsed = 0;
-    u64 OSWaitTime = OSFreq * MillisecondsToWait / 1000;
-    while(OSElapsed < OSWaitTime) {
-        OSEnd = ReadOSTimer();
-        OSElapsed = OSEnd - OSStart;
+    u64 block_start = READ_BLOCK_TIMER();
+    u64 os_start = read_os_timer();
+    u64 os_end = 0;
+    u64 os_elapsed = 0;
+    u64 os_wait_time = os_freq * ms_to_wait / 1000;
+    while(os_elapsed < os_wait_time) {
+        os_end = read_os_timer();
+        os_elapsed = os_end - os_start;
     }
 
-    u64 BlockEnd = READ_BLOCK_TIMER();
-    u64 BlockElapsed = BlockEnd - BlockStart;
+    u64 block_end = READ_BLOCK_TIMER();
+    u64 block_elapsed = block_end - block_start;
 
-    u64 BlockFreq = 0;
-    if(OSElapsed) {
-        BlockFreq = OSFreq * BlockElapsed / OSElapsed;
+    u64 block_freq = 0;
+    if(os_elapsed) {
+        block_freq = os_freq * block_elapsed / os_elapsed;
     }
 
-    return BlockFreq;
+    return block_freq;
 }
 
-static void BeginProfile(void) {
-    GlobalProfiler.StartTSC = READ_BLOCK_TIMER();
+static void begin_profile(void) {
+    g_profiler.start_TSC = READ_BLOCK_TIMER();
 }
 
-static void EndAndPrintProfile() {
-    GlobalProfiler.EndTSC = READ_BLOCK_TIMER();
-    u64 TimerFreq = EstimateBlockTimerFreq();
+static void end_profile() {
+    g_profiler.end_TSC = READ_BLOCK_TIMER();
+    u64 freq = estimate_block_freq();
 
-    u64 TotalTSCElapsed = GlobalProfiler.EndTSC - GlobalProfiler.StartTSC;
+    u64 total_elapsed = g_profiler.end_TSC - g_profiler.start_TSC;
 
-    if(TimerFreq) {
-        printf("\nTotal time: %0.4fms (timer freq %llu)\n", 1000.0 * (double)TotalTSCElapsed / (double)TimerFreq, TimerFreq);
+    if(freq) {
+        printf("\nTotal time: %0.4fms (timer freq %llu)\n", 1000.0 * (double)total_elapsed / (double)freq, freq);
     }
 
-    PrintAnchorData(TotalTSCElapsed, TimerFreq);
+    print_anchor(total_elapsed, freq);
 }
 
 } // namespace blast
