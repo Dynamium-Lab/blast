e667153ba05fd58d8049db526dc4c17ecd23614d
Nikos Doiron <end7720@umoncton.ca>
2026-02-19 12:35:30 -0400

    Bug fixes
    - added collision to ConstraintType
    - converted scenes to current blast nomenclature
    - fixed test_functions for current blast nomenclature

--- blast/blast_optimization.hpp
+++ blast/blast_optimization.hpp
@@ -4,152 +4,171 @@
 #include <cstring>
 #include <functional>
 
-namespace blast {
+namespace blast
+{
+  struct Optimization;
+  struct Objective;
+  struct ConstraintSelection;
+  struct Guess;
 
-struct Optimization;
-struct Objective;
-struct ConstraintSelection;
-struct Guess;
-
-using ConstraintFunctionVector = std::vector<void (*)(double* result, Optimization* opt)>;
-using ConstraintFunction       = void (*)(double*, Optimization*);
+  using ConstraintFunctionVector = std::vector<void (*)(double* result, Optimization* opt)>;
+  using ConstraintFunction = void (*)(double*, Optimization*);
 
 
-using ObjectiveFunction = double (*)(Optimization* opt);
+  using ObjectiveFunction = double (*)(Optimization* opt);
 
-struct Objective {
-  real K_time = 1.0;
-  // real K_energy = 0; // not supported atm
-  // real K_jerk = 0; // not supported atm
-  // real K_obstacle_avoidance = 0; // not supported atm
+  struct Objective
+  {
+    real K_time = 1.0;
+    // real K_energy = 0; // not supported atm
+    // real K_jerk = 0; // not supported atm
+    // real K_obstacle_avoidance = 0; // not supported atm
 
-  // Setup function to return the NLopt-compatible objective function
-  std::vector<real>                    k_extra_objectives{};
-  std::vector<real (*)(Optimization*)> extra_objectives{};
+    // Setup function to return the NLopt-compatible objective function
+    std::vector<real> k_extra_objectives{};
+    std::vector<real (*)(Optimization*)> extra_objectives{};
 
-  inline void add_custom_objective(ObjectiveFunction function, real k);
-};
-
-struct Guess {
-  enum GuessType : u32 {
-    custom,
-    random,
-    from_list,
-    rrt_connect
+    inline void add_custom_objective(ObjectiveFunction function, real k);
   };
-  GuessType type      = random;
-  real      parameter = 0;
-  u32       n_shot    = 100;
-  Array     x0;
-  Matrix    candidates;
 
-  Guess() = default;
+  struct Guess
+  {
+    enum GuessType : u32
+    {
+      custom,
+      random,
+      from_list,
+      rrt_connect
+    };
 
-  // Constructor for Guess::custom, initializing x0
-  explicit Guess(Array x_0) :
+    GuessType type = random;
+    real parameter = 0;
+    u32 n_shot = 100;
+    Array x0;
+    Matrix candidates;
+
+    Guess() = default;
+
+    // Constructor for Guess::custom, initializing x0
+    explicit Guess(Array x_0) :
       type(Guess::custom),
-      x0(std::move(x_0)) {}
+      x0(std::move(x_0))
+    {
+    }
 
-  // Constructor for Guess::from_list, initializing candidates
-  explicit Guess(Matrix m) :
+    // Constructor for Guess::from_list, initializing candidates
+    explicit Guess(Matrix m) :
       type(Guess::from_list),
-      candidates(std::move(m)) {}
+      candidates(std::move(m))
+    {
+    }
 
-  // Constructor for Guess::random, initializing n_shot
-  explicit Guess(u32 shots) :
+    // Constructor for Guess::random, initializing n_shot
+    explicit Guess(u32 shots) :
       type(Guess::random),
-      n_shot(shots) {}
+      n_shot(shots)
+    {
+    }
 
-  // Constructor for Guess::rrt_connect, initializing parameter
-  explicit Guess(real param) :
+    // Constructor for Guess::rrt_connect, initializing parameter
+    explicit Guess(real param) :
       type(Guess::rrt_connect),
-      parameter(param) {}
-};
+      parameter(param)
+    {
+    }
+  };
 
-enum ConstraintType : unsigned {
-  position     = 1 << 0,
-  velocity     = 1 << 1,
-  acceleration = 1 << 2,
-};
+  enum ConstraintType : unsigned
+  {
+    position = 1 << 0,
+    velocity = 1 << 1,
+    acceleration = 1 << 2,
+    collision = 1 << 3,
+  };
 
-struct ConstraintSelection {
-  bool position            = false;
-  bool velocity            = false;
-  bool acceleration        = false;
-  bool torque              = false;
-  bool jerk                = false; // not supported atm
-  bool tcp_speed           = false;
-  bool self_collisions     = false;
-  bool external_collisions = false;
+  struct ConstraintSelection
+  {
+    bool position = false;
+    bool velocity = false;
+    bool acceleration = false;
+    bool torque = false;
+    bool jerk = false; // not supported atm
+    bool tcp_speed = false;
+    bool self_collisions = false;
+    bool external_collisions = false;
 
-  int n_collision_constraints   = 5; // todo: remove because of new paradigm
-  int n_collision_skip          = 2; // todo: remove because of new paradigm
-  int n_constraints             = 0;
-  int n_constraints_per_segment = 0;
+    int n_collision_constraints = 5; // todo: remove because of new paradigm
+    int n_collision_skip = 2; // todo: remove because of new paradigm
+    int n_constraints = 0;
+    int n_constraints_per_segment = 0;
 
-  // Added more info for testing
-  bool                show_info = false;
-  std::vector<Matrix> grad_list;
-  std::vector<Array>  constr_list;
-  std::vector<Array>  x_list;
+    // Added more info for testing
+    bool show_info = false;
+    std::vector<Matrix> grad_list;
+    std::vector<Array> constr_list;
+    std::vector<Array> x_list;
 
-  ConstraintFunctionVector extra_constraints   = {};
-  std::vector<u32>         n_extra_constraints = {};
-  void                     add_constraint(ConstraintFunction, int n_con);
-};
+    ConstraintFunctionVector extra_constraints = {};
+    std::vector<u32> n_extra_constraints = {};
+    void add_constraint(ConstraintFunction, int n_con);
+  };
 
-struct Optimization {
-  Manipulator         manip;
-  Bspline             bspline;
-  Guess               guess;
-  ConstraintSelection constraints;
-  Objective           objective;
-  Matrix              task;
-  World               world;
-  real                trajectory_start_time = 0.0;
-  real                success_tolerance     = 0.0; // % of constraint violation after optimization that is still considered a success
-  int                 max_tries             = 1;   // Maximum number of tries in the optimization loop.
+  struct Optimization
+  {
+    Manipulator manip;
+    Bspline bspline;
+    Guess guess;
+    ConstraintSelection constraints;
+    Objective objective;
+    Matrix task;
+    World world;
+    real trajectory_start_time = 0.0;
+    real success_tolerance = 0.0; // % of constraint violation after optimization that is still considered a success
+    int max_tries = 1; // Maximum number of tries in the optimization loop.
 
-  void* custom_data;
+    void* custom_data;
 
-  Array lb;
-  Array ub;
+    Array lb;
+    Array ub;
 
-  Optimization() = delete;
+    Optimization() = delete;
 
-  Optimization(const Manipulator& new_manip, const Matrix& new_task);
+    Optimization(const Manipulator& new_manip, const Matrix& new_task);
 
-  Optimization(const Manipulator& new_manip, const Matrix& new_task, const Bspline& new_bspline);
+    Optimization(const Manipulator& new_manip, const Matrix& new_task, const Bspline& new_bspline);
 
-  int x_len() const;
+    int x_len() const;
 
-  void set_manip(Manipulator new_manip);
-  void set_bspline(Bspline new_bspline);
-  void set_guess(Guess new_guess);
-  void set_constraints(ConstraintSelection new_constraints);
-  void set_objective(Objective new_objective);
-  void set_task(Matrix new_task);
-  void set_world(World new_world);
-};
+    void set_manip(Manipulator new_manip);
+    void set_bspline(Bspline new_bspline);
+    void set_guess(Guess new_guess);
+    void set_constraints(ConstraintSelection new_constraints);
+    void set_objective(Objective new_objective);
+    void set_task(Matrix new_task);
+    void set_world(World new_world);
+  };
 
-inline void constraints_and_gradients_with_segments(const Array& x, Optimization& opt, Array& constraints, Matrix& grad);
-// inline void compute_constraints_with_segments(const Array& x, Optimization& opt, Array& constraints);
-inline void nlopt_constraints_with_segments(unsigned m, double* result, unsigned x_len, const double* x, double* grad, void* f_data);
+  inline void constraints_and_gradients_with_segments(const Array& x, Optimization& opt, Array& constraints,
+                                                      Matrix& grad);
+  // inline void compute_constraints_with_segments(const Array& x, Optimization& opt, Array& constraints);
+  inline void nlopt_constraints_with_segments(unsigned m, double* result, unsigned x_len, const double* x, double* grad,
+                                              void* f_data);
 
-inline void   compute_constraints(double* result, const Array& x, Optimization* opt);
-inline void   nlopt_constraints(unsigned m, double* result, unsigned x_len, const double* x, double* grad, void* f_data);
-inline double compute_objective(Array& current_x, Optimization* opt);
-inline bool   validate_task(Optimization* opt);
+  inline void compute_constraints(double* result, const Array& x, Optimization* opt);
+  inline void nlopt_constraints(unsigned m, double* result, unsigned x_len, const double* x, double* grad,
+                                void* f_data);
+  inline double compute_objective(Array& current_x, Optimization* opt);
+  inline bool validate_task(Optimization* opt);
 
-inline void nlopt_constraints_dev(unsigned m, double* result, unsigned xlen, const double* x, double* grad, void* f_data);
-inline void nlopt_constraints_dev_new(unsigned m, double* result, unsigned xlen, const double* x, double* grad, void* f_data);
-
-
-// inline real   bound_constraint(const real& value, const real& value_min, const real& value_max);
-// inline Matrix get_J_tool(const Optimization* opt);
-// inline bool   validate_task(Optimization* opt);
+  inline void nlopt_constraints_dev(unsigned m, double* result, unsigned xlen, const double* x, double* grad,
+                                    void* f_data);
+  inline void nlopt_constraints_dev_new(unsigned m, double* result, unsigned xlen, const double* x, double* grad,
+                                        void* f_data);
 
 
+  // inline real   bound_constraint(const real& value, const real& value_min, const real& value_max);
+  // inline Matrix get_J_tool(const Optimization* opt);
+  // inline bool   validate_task(Optimization* opt);
 } // namespace blast
 
 #include "optimization/constraints.hpp"
--- tests/test_helper/scenes.hpp
+++ tests/test_helper/scenes.hpp
@@ -1,103 +1,110 @@
+#pragma once
 #include <blast>
 
 // Extracted scenes from MotionBenchMaker (github : https://github.com/KavrakiLab/motion_bench_maker)
 
-inline host_fn blast::World get_bookshelf_small() {
+inline host_fn blast::World get_bookshelf_small()
+{
     blast::World bookshelf_small;
-    blast::add_static_capsule({0.9, 0.0, 1.01}, {0.9, 0.0, 1.15},  0.03, &bookshelf_small);
-    blast::add_static_capsule({0.7, 0.0, 1.01}, {0.7, 0.0, 1.15},  0.03, &bookshelf_small);
-    blast::add_static_capsule({0.5, 0.0, 1.01}, {0.5, 0.0, 1.15},  0.03, &bookshelf_small);
-    blast::add_static_box({1, 0, 1}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_small);
-    blast::add_static_box({1, -0.5, 1.15}, {0.6, 0.02, 0.17}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_small);
-    blast::add_static_box({1, 0.5, 1.15}, {0.6, 0.02, 0.17}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_small);
-    blast::add_static_box({1, 0, 1.3}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_small);
+    bookshelf_small.add_capsule({0.9, 0.0, 1.01}, {0.9, 0.0, 1.15}, 0.03);
+    bookshelf_small.add_capsule({0.7, 0.0, 1.01}, {0.7, 0.0, 1.15}, 0.03);
+    bookshelf_small.add_capsule({0.5, 0.0, 1.01}, {0.5, 0.0, 1.15}, 0.03);
+    bookshelf_small.add_box({1, 0, 1}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_small.add_box({1, -0.5, 1.15}, {0.6, 0.02, 0.17}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_small.add_box({1, 0.5, 1.15}, {0.6, 0.02, 0.17}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_small.add_box({1, 0, 1.3}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
     return bookshelf_small;
 }
 
-inline host_fn blast::World get_bookshelf_tall() {
+inline host_fn blast::World get_bookshelf_tall()
+{
     blast::World bookshelf_tall;
-    blast::add_static_capsule({0.9, 0.0, 1.31}, {0.9, 0.0, 1.45},  0.03, &bookshelf_tall);
-    blast::add_static_capsule({0.7, 0.0, 1.31}, {0.7, 0.0, 1.45},  0.03, &bookshelf_tall);
-    blast::add_static_capsule({0.5, 0.0, 1.31}, {0.5, 0.0, 1.45},  0.03, &bookshelf_tall);
-    blast::add_static_capsule({0.9, 0.0, 0.71}, {0.9, 0.0, 0.85},  0.03, &bookshelf_tall);
-    blast::add_static_capsule({0.7, 0.0, 0.71}, {0.7, 0.0, 0.85},  0.03, &bookshelf_tall);
-    blast::add_static_capsule({0.5, 0.0, 0.71}, {0.5, 0.0, 0.85},  0.03, &bookshelf_tall);
-    blast::add_static_capsule({0.9, 0.0, 1.01}, {0.9, 0.0, 1.15},  0.03, &bookshelf_tall);
-    blast::add_static_capsule({0.7, 0.0, 1.01}, {0.7, 0.0, 1.15},  0.03, &bookshelf_tall);
-    blast::add_static_capsule({0.5, 0.0, 1.01}, {0.5, 0.0, 1.15},  0.03, &bookshelf_tall);
-    blast::add_static_box({1.0, 0, 0.7}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_tall);
-    blast::add_static_box({1.0, 0, 1.0}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_tall);
-    blast::add_static_box({1.0, 0, 1.3}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_tall);
-    blast::add_static_box({1, 0, 1.6}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_tall);
-    blast::add_static_box({1, -0.5, 0.82}, {0.65, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_tall);
-    blast::add_static_box({1, 0.5, 0.82}, {0.65, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_tall);
+    bookshelf_tall.add_capsule({0.9, 0.0, 1.31}, {0.9, 0.0, 1.45}, 0.03);
+    bookshelf_tall.add_capsule({0.7, 0.0, 1.31}, {0.7, 0.0, 1.45}, 0.03);
+    bookshelf_tall.add_capsule({0.5, 0.0, 1.31}, {0.5, 0.0, 1.45}, 0.03);
+    bookshelf_tall.add_capsule({0.9, 0.0, 0.71}, {0.9, 0.0, 0.85}, 0.03);
+    bookshelf_tall.add_capsule({0.7, 0.0, 0.71}, {0.7, 0.0, 0.85}, 0.03);
+    bookshelf_tall.add_capsule({0.5, 0.0, 0.71}, {0.5, 0.0, 0.85}, 0.03);
+    bookshelf_tall.add_capsule({0.9, 0.0, 1.01}, {0.9, 0.0, 1.15}, 0.03);
+    bookshelf_tall.add_capsule({0.7, 0.0, 1.01}, {0.7, 0.0, 1.15}, 0.03);
+    bookshelf_tall.add_capsule({0.5, 0.0, 1.01}, {0.5, 0.0, 1.15}, 0.03);
+    bookshelf_tall.add_box({1.0, 0, 0.7}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_tall.add_box({1.0, 0, 1.0}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_tall.add_box({1.0, 0, 1.3}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_tall.add_box({1, 0, 1.6}, {0.6, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_tall.add_box({1, -0.5, 0.82}, {0.65, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_tall.add_box({1, 0.5, 0.82}, {0.65, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
     return bookshelf_tall;
 }
 
-inline host_fn blast::World get_bookshelf_thin() {
+inline host_fn blast::World get_bookshelf_thin()
+{
     blast::World bookshelf_thin;
-    blast::add_static_capsule({1.1, 0.25, 0.37}, {1.1, 0.25, 0.51},  0.03, &bookshelf_thin);
-    blast::add_static_capsule({0.8, 0.25, 0.37}, {0.8, 0.25, 0.51},  0.03, &bookshelf_thin);
-    blast::add_static_capsule({1.1, -0.25, 0.62}, {1.1, -0.25, 0.76},  0.03, &bookshelf_thin);
-    blast::add_static_capsule({0.8, -0.25, 0.62}, {0.8, -0.25, 0.76},  0.03, &bookshelf_thin);
-    blast::add_static_capsule({1.1, 0.25, 0.87}, {1.1, 0.25, 1.01},  0.03, &bookshelf_thin);
-    blast::add_static_capsule({0.8, 0.25, 0.87}, {0.8, 0.25, 1.01},  0.03, &bookshelf_thin);
-    blast::add_static_capsule({1.1, -0.25, 1.12}, {1.1, -0.25, 1.26},  0.03, &bookshelf_thin);
-    blast::add_static_capsule({0.8, -0.25, 1.12}, {0.8, -0.25, 1.26},  0.03, &bookshelf_thin);
-    blast::add_static_capsule({1.1, 0.25, 1.37}, {1.1, 0.25, 1.51},  0.03, &bookshelf_thin);
-    blast::add_static_capsule({0.8, 0.25, 1.37}, {0.8, 0.25, 1.51},  0.03, &bookshelf_thin);
-    blast::add_static_box({1.0, 0.0, 0.35}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_thin);
-    blast::add_static_box({1.0, 0, 0.6}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_thin);
-    blast::add_static_box({1.0, 0, 0.85}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_thin);
-    blast::add_static_box({1, 0, 1.1}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_thin);
-    blast::add_static_box({1, 0, 1.35}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_thin);
-    blast::add_static_box({1, 0, 0.9}, {0.31, 0.02, 0.7}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_thin);
-    blast::add_static_box({1, 0, 1.6}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_thin);
-    blast::add_static_box({0.7, 0.5, 0.82}, {0.02, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_thin);
-    blast::add_static_box({0.7, -0.5, 0.82}, {0.02, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_thin);
-    blast::add_static_box({1.3, 0.5, 0.82}, {0.02, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_thin);
-    blast::add_static_box({1.3, -0.5, 0.82}, {0.02, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &bookshelf_thin);
+    bookshelf_thin.add_capsule({1.1, 0.25, 0.37}, {1.1, 0.25, 0.51}, 0.03);
+    bookshelf_thin.add_capsule({0.8, 0.25, 0.37}, {0.8, 0.25, 0.51}, 0.03);
+    bookshelf_thin.add_capsule({1.1, -0.25, 0.62}, {1.1, -0.25, 0.76}, 0.03);
+    bookshelf_thin.add_capsule({0.8, -0.25, 0.62}, {0.8, -0.25, 0.76}, 0.03);
+    bookshelf_thin.add_capsule({1.1, 0.25, 0.87}, {1.1, 0.25, 1.01}, 0.03);
+    bookshelf_thin.add_capsule({0.8, 0.25, 0.87}, {0.8, 0.25, 1.01}, 0.03);
+    bookshelf_thin.add_capsule({1.1, -0.25, 1.12}, {1.1, -0.25, 1.26}, 0.03);
+    bookshelf_thin.add_capsule({0.8, -0.25, 1.12}, {0.8, -0.25, 1.26}, 0.03);
+    bookshelf_thin.add_capsule({1.1, 0.25, 1.37}, {1.1, 0.25, 1.51}, 0.03);
+    bookshelf_thin.add_capsule({0.8, 0.25, 1.37}, {0.8, 0.25, 1.51}, 0.03);
+    bookshelf_thin.add_box({1.0, 0.0, 0.35}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_thin.add_box({1.0, 0, 0.6}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_thin.add_box({1.0, 0, 0.85}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_thin.add_box({1, 0, 1.1}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_thin.add_box({1, 0, 1.35}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_thin.add_box({1, 0, 0.9}, {0.31, 0.02, 0.7}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_thin.add_box({1, 0, 1.6}, {0.31, 0.5, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_thin.add_box({0.7, 0.5, 0.82}, {0.02, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_thin.add_box({0.7, -0.5, 0.82}, {0.02, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_thin.add_box({1.3, 0.5, 0.82}, {0.02, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    bookshelf_thin.add_box({1.3, -0.5, 0.82}, {0.02, 0.02, 0.82}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
     return bookshelf_thin;
 }
 
-inline host_fn blast::World get_scene_box() {
+inline host_fn blast::World get_scene_box()
+{
     blast::World box;
-    blast::add_static_capsule({0.8, 0.0, 0.48}, {0.8, 0.0, 0.62},  0.03, &box);
-    blast::add_static_box({0.8, 0, 0.44}, {0.35, 0.35, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &box);
-    blast::add_static_box({0.8, -0.35, 0.8}, {0.35, 0.02, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &box);
-    blast::add_static_box({0.8, 0.35, 0.8}, {0.35, 0.02, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &box);
-    blast::add_static_box({0.45, 0, 0.7}, {0.02, 0.35, 0.3}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &box);
-    blast::add_static_box({0.9, 0, 1.35}, {0.35, 0.35, 0.02}, {-0.708, 0.0, 0.708, 0.0, -1.001, 0.0, 0.708, 0.0, 0.707}, &box);
-    blast::add_static_box({1.15, 0, 0.8}, {0.02, 0.35, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &box);
+    box.add_capsule({0.8, 0.0, 0.48}, {0.8, 0.0, 0.62}, 0.03);
+    box.add_box({0.8, 0, 0.44}, {0.35, 0.35, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    box.add_box({0.8, -0.35, 0.8}, {0.35, 0.02, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    box.add_box({0.8, 0.35, 0.8}, {0.35, 0.02, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    box.add_box({0.45, 0, 0.7}, {0.02, 0.35, 0.3}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    box.add_box({0.9, 0, 1.35}, {0.35, 0.35, 0.02}, {-0.708, 0.0, 0.708, 0.0, -1.001, 0.0, 0.708, 0.0, 0.707});
+    box.add_box({1.15, 0, 0.8}, {0.02, 0.35, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
     return box;
 }
 
-inline host_fn blast::World get_scene_cage() {
+inline host_fn blast::World get_scene_cage()
+{
     blast::World cage;
-    blast::add_static_box({0.8, 0, 0.52}, {0.035, 0.035, 0.035}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &cage);
-    blast::add_static_box({0.8, 0, 0.44}, {0.35, 0.35, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &cage);
-    blast::add_static_box({0.8, -0.35, 0.8}, {0.35, 0.02, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &cage);
-    blast::add_static_box({0.8, 0.35, 0.8}, {0.35, 0.02, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &cage);
-    blast::add_static_box({0.45, 0, 0.9}, {0.02, 0.35, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &cage);
-    blast::add_static_box({0.45, 0, 0.6}, {0.02, 0.35, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &cage);
-    blast::add_static_box({0.85, 0, 1.15}, {0.325, 0.325, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &cage);
-    blast::add_static_box({1.15, 0, 0.8}, {0.02, 0.35, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &cage);
+    cage.add_box({0.8, 0, 0.52}, {0.035, 0.035, 0.035}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    cage.add_box({0.8, 0, 0.44}, {0.35, 0.35, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    cage.add_box({0.8, -0.35, 0.8}, {0.35, 0.02, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    cage.add_box({0.8, 0.35, 0.8}, {0.35, 0.02, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    cage.add_box({0.45, 0, 0.9}, {0.02, 0.35, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    cage.add_box({0.45, 0, 0.6}, {0.02, 0.35, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    cage.add_box({0.85, 0, 1.15}, {0.325, 0.325, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    cage.add_box({1.15, 0, 0.8}, {0.02, 0.35, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
     return cage;
 }
 
-inline host_fn blast::World get_scene_table() {
+inline host_fn blast::World get_scene_table()
+{
     blast::World table;
-    blast::add_static_capsule({0.85, 0.0, 0.74}, {0.85, 0.0, 0.86},  0.03, &table);
-    blast::add_static_box({0.75, 0.4, 0.85}, {0.125, 0.125, 0.125}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &table);
-    blast::add_static_box({1.5, 0.85, 0.35}, {0.025, 0.025, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &table);
-    blast::add_static_box({0.6, 0.85, 0.35}, {0.025, 0.025, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &table);
-    blast::add_static_box({1.5, -0.85, 0.35}, {0.025, 0.025, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &table);
-    blast::add_static_box({0.6, -0.85, 0.35}, {0.025, 0.025, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &table);
-    blast::add_static_box({1.05, 0, 0.7}, {0.6, 1.0, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &table);
-    blast::add_static_capsule({1.35, 0.0, 0.675}, {1.35, 0.0, 1.025},  0.05, &table);
-    blast::add_static_box({1.05, -0.2, 0.9}, {0.1, 0.01, 0.2}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &table);
-    blast::add_static_box({0.65, 0.2, 0.9}, {0.01, 0.1, 0.2}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &table);
-    blast::add_static_box({0.65, -0.2, 0.9}, {0.1, 0.025, 0.175}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &table);
-    blast::add_static_box({1.05, 0.2, 0.9}, {0.1, 0.025, 0.175}, {-1, 0, 0, 0, -1, 0, 0, 0, 1}, &table);
+    table.add_capsule({0.85, 0.0, 0.74}, {0.85, 0.0, 0.86}, 0.03);
+    table.add_box({0.75, 0.4, 0.85}, {0.125, 0.125, 0.125}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    table.add_box({1.5, 0.85, 0.35}, {0.025, 0.025, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    table.add_box({0.6, 0.85, 0.35}, {0.025, 0.025, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    table.add_box({1.5, -0.85, 0.35}, {0.025, 0.025, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    table.add_box({0.6, -0.85, 0.35}, {0.025, 0.025, 0.35}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    table.add_box({1.05, 0, 0.7}, {0.6, 1.0, 0.02}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    table.add_capsule({1.35, 0.0, 0.675}, {1.35, 0.0, 1.025}, 0.05);
+    table.add_box({1.05, -0.2, 0.9}, {0.1, 0.01, 0.2}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    table.add_box({0.65, 0.2, 0.9}, {0.01, 0.1, 0.2}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    table.add_box({0.65, -0.2, 0.9}, {0.1, 0.025, 0.175}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
+    table.add_box({1.05, 0.2, 0.9}, {0.1, 0.025, 0.175}, {-1, 0, 0, 0, -1, 0, 0, 0, 1});
     return table;
 }
