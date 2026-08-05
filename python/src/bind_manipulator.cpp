c553444c2226a3f53f16d1ea1feb1ad7b218c27e
ThomasDuquette <133025159+ThomasDuquette@users.noreply.github.com>
2026-08-05 09:23:50 -0300

    bug fic

--- python/src/bind_manipulator.cpp
+++ python/src/bind_manipulator.cpp
@@ -116,7 +116,7 @@ void bind_manipulator(nb::module_& m) {
                   "capsule_list",
                   [](const ManipulatorCapsules& c) {
                     nb::list lst;
-                    for (const auto& capsule: c._collision_model)
+                    for (const auto& capsule: c.capsule_list)
                       lst.append(capsule);
                     return lst;
                   },
@@ -127,7 +127,7 @@ void bind_manipulator(nb::module_& m) {
                       if (i >= MAX_CAPSULES)
                         throw nb::value_error("Too many capsules");
 
-                      c._collision_model[i++] = nb::cast<CollisionModelCapsule>(item);
+                      c.capsule_list[i++] = nb::cast<CollisionModelCapsule>(item);
                     }
                   })
           .def_prop_rw("collision_base", [](const ManipulatorCapsules& c) { return array_to_copy(c.collision_base); }, [](ManipulatorCapsules& c, nb::ndarray<nb::numpy, real, nb::ndim<1>, nb::c_contig> a) { c.collision_base = array_from_numpy(a); }, nb::rv_policy::move, "Boolean-like array: 1 if capsule[i] can collide with the base sphere.")
